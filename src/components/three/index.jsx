b25d7c4e6427426f4bc14cd21c8b149f5719cd4f
Niltonsf <nilton.schumacher.filho@gmail.com>
2022-05-23 21:53:46 -0300

    Playing with cameras and mesh

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,16 +1,31 @@
-import { PerspectiveCamera } from '@react-three/drei';
+import React, { useRef } from 'react';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useEffect } from 'react';
 import { angleToRadians } from '../../utils/angles';
 
 export default function Three() {
+
+	const orbitControlsRef = useRef(null)
+	useFrame((state) => {
+		if (!!orbitControlsRef.current) {
+			const { x, y } = state.mouse;
+			orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(90));
+			orbitControlsRef.current.setPolarAngle((y + 0.9) * angleToRadians(60));
+			orbitControlsRef.current.update();
+		}
+	});
+
 	return (
 		<>
 			{/* Camera */}
-			<PerspectiveCamera makeDefault position={[0, 1, 5]}/>
+			<PerspectiveCamera makeDefault position={[0, 1, 8]}/>
 
+			<OrbitControls ref={orbitControlsRef} maxPolarAngle={angleToRadians(80)} minPolarAngle={angleToRadians(30)}/>
 			{/* Ball */}
 			<mesh position={[0, 0.5, 0]}>
 				<sphereGeometry args={[0.5, 32, 32]} />
-				<meshStandardMaterial color="#ffffff"/>
+				<meshStandardMaterial color="#f1f1f1"/>
 			</mesh>
 
 			{/* Floor */}
@@ -20,7 +35,7 @@ export default function Three() {
 			</mesh>
 
 			{/* Ambient Light */}
-			<ambientLight args={["#fff000", 1]}/>
+			<ambientLight args={["#ffffff", 1]}/>
 		</>
 	);
 }
