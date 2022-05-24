d30b5faf99af3ea74fab520e97fe066ff240717f
Niltonsf <nilton.schumacher.filho@gmail.com>
2022-05-24 17:20:16 -0300

    Lights, shadows and enviroment

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useEffect } from 'react';
 import { angleToRadians } from '../../utils/angles';
+import * as THREE from "three";
 
 export default function Three() {
 
@@ -21,21 +21,41 @@ export default function Three() {
 			{/* Camera */}
 			<PerspectiveCamera makeDefault position={[0, 1, 8]}/>
 
-			<OrbitControls ref={orbitControlsRef} maxPolarAngle={angleToRadians(80)} minPolarAngle={angleToRadians(30)}/>
+			<OrbitControls 
+				ref={orbitControlsRef} 
+				maxPolarAngle={angleToRadians(80)} 
+				minPolarAngle={angleToRadians(30)}
+			/>
+
 			{/* Ball */}
-			<mesh position={[0, 0.5, 0]}>
+			<mesh position={[0, 0.5, 0]} castShadow>
 				<sphereGeometry args={[0.5, 32, 32]} />
-				<meshStandardMaterial color="#f1f1f1"/>
+				<meshStandardMaterial color="#ffffff" metalness={0.5} roughness={0.3}/>
 			</mesh>
 
 			{/* Floor */}
-			<mesh rotation={[-(angleToRadians(90)), 0, 0]}>
-				<planeGeometry args={[6, 6]} />
-				<meshStandardMaterial color="#A020F0"/>
+			<mesh rotation={[-(angleToRadians(90)), 0, 0]} receiveShadow>
+				<planeGeometry args={[20, 20]} />
+				<meshStandardMaterial color="#1ea3d8" />
 			</mesh>
 
 			{/* Ambient Light */}
-			<ambientLight args={["#ffffff", 1]}/>
+			<ambientLight args={["#ffffff", 0.25]}/>
+
+			{/* Spot light */}
+			<spotLight 
+				args={["#ffffff", 0.6, 0, angleToRadians(20), 0.3]} 
+				position={[-6, 4, 0]} 
+				castShadow 
+			/>
+
+			{/* Enviroment */}
+			<Environment background>
+				<mesh scale={100}>
+					<sphereGeometry args={[1, 64, 64]} />
+					<meshBasicMaterial side={THREE.BackSide} color="#2266cc"/>
+				</mesh>
+			</Environment>
 		</>
 	);
 }
